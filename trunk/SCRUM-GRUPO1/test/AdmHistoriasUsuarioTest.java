3b02b3b6c31eeb83cb366d81ceb5ddc44e7d747d
evy2586@gmail.com <evy2586@gmail.com@4e4c0a45-7b8d-c99e-a0c6-614e66cbec3a>
2012-03-25 04:55:20 +0000

    creacion de historias de usuario

---
+++ trunk/SCRUM-GRUPO1/test/AdmHistoriasUsuarioTest.java
@@ -0,0 +1,35 @@
+package upc.edu.pe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pcsiecon
+ */
+public class AdmHistoriasUsuarioTest {
+
+    public AdmHistoriasUsuarioTest() {
+    }
+
+    @Test
+    public void admHistoriasDebeRegistrarHistoriaUsuario(){
+        // Arrange
+        //   Definir los datos de pruebas
+        String codigo = "HU001";
+        String descripcion = "Realizar el requerimiento N1";
+        String fecha = "24/03/2012";
+        // Act
+        //  Ejecutar los métodos a probar
+        AdmHistoriasUsuario adm = new AdmHistoriasUsuario();
+        adm.registrarHistoriaUsuario(codigo, descripcion, fecha);
+        // Assert
+        HistoriaUsuario historia = adm.buscarHistoria(codigo);
+        assertNotNull(historia);
+        assertTrue(historia.getCodigo().equals(codigo));
+        assertTrue(historia.getDescripcion().equals(descripcion));
+        assertTrue(historia.getFecha().equals(fecha));
+    }
+
+
+}
