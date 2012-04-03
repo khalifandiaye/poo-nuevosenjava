3b9d8ee81eee5bbbabf8d035ca3a339bbe4762fc
evy2586 <evy2586@4e4c0a45-7b8d-c99e-a0c6-614e66cbec3a>
2012-04-03 02:39:36 +0000

    se agrego campos a la clase HistoriaUsuario

--- trunk/SCRUM-GRUPO1/src/gestionscrum/HistoriaUsuario/HistoriaUsuario.java
+++ trunk/SCRUM-GRUPO1/src/gestionscrum/HistoriaUsuario/HistoriaUsuario.java
@@ -1,18 +1,22 @@
 package gestionscrum.HistoriaUsuario;
 /**
  *
- * @author pcsiecon
+ * @author Evelyn Cordova
  */
 public class HistoriaUsuario {
     private String codigo;
     private String descripcion;
     private String fecha;
+    private String usuario;
+    private String estado;
 
 
-    public HistoriaUsuario(String codigo, String descripcion, String fecha) {
+    public HistoriaUsuario(String codigo, String descripcion, String fecha,String usuario, String estado) {
         this.codigo = codigo;
         this.descripcion = descripcion;
         this.fecha = fecha;
+        this.usuario= usuario;
+        this.estado = estado;
     }
 
     /**
@@ -55,6 +59,34 @@ public class HistoriaUsuario {
      */
     public void setFecha(String fecha) {
         this.fecha = fecha;
+    }
+    
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param fecha the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param fecha the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 
 }
