112576f17edc3f685fc68d8687f5a22b422aa644
evy2586 <evy2586@4e4c0a45-7b8d-c99e-a0c6-614e66cbec3a>
2012-04-03 02:39:52 +0000

    se agrego campos a la clase HistoriaUsuario

--- SCRUM-GRUPO1/test/gestionscrum/HistoriaUsuario/AdmHistoriasUsuarioTest.java
+++ SCRUM-GRUPO1/test/gestionscrum/HistoriaUsuario/AdmHistoriasUsuarioTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author pcsiecon
+ * @author Evelyn Córdova
  */
 public class AdmHistoriasUsuarioTest {
 
@@ -22,16 +22,20 @@ public class AdmHistoriasUsuarioTest {
         String codigo = "HU001";
         String descripcion = "Realizar el requerimiento N1";
         String fecha = "24/03/2012";
+        String usuario = "P0001";
+        String estado = "Asignado";
         // Act
         //  Ejecutar los métodos a probar
         AdmHistoriasUsuario adm = new AdmHistoriasUsuario();
-        adm.registrarHistoriaUsuario(codigo, descripcion, fecha);
+        adm.registrarHistoriaUsuario(codigo, descripcion, fecha,usuario, estado);
         // Assert
         HistoriaUsuario historia = adm.buscarHistoria(codigo);
         assertNotNull(historia);
         assertTrue(historia.getCodigo().equals(codigo));
         assertTrue(historia.getDescripcion().equals(descripcion));
         assertTrue(historia.getFecha().equals(fecha));
+        assertTrue(historia.getUsuario().equals(usuario));
+        assertTrue(historia.getEstado().equals(estado));
     }
 
 
