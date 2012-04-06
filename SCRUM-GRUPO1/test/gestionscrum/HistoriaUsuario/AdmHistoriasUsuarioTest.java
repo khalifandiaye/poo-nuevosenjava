839240e764efc192cba59f7f8474e23a9682728c
evy2586 <evy2586@4e4c0a45-7b8d-c99e-a0c6-614e66cbec3a>
2012-04-06 03:14:07 +0000

    Actualizacion de la clase

--- SCRUM-GRUPO1/test/gestionscrum/HistoriaUsuario/AdmHistoriasUsuarioTest.java
+++ SCRUM-GRUPO1/test/gestionscrum/HistoriaUsuario/AdmHistoriasUsuarioTest.java
@@ -1,42 +1,58 @@
 package gestionscrum.HistoriaUsuario;
 
 import gestionscrum.HistoriaUsuario.AdmHistoriasUsuario;
-import gestionscrum.HistoriaUsuario.HistoriaUsuario;
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * 
  * @author Evelyn Córdova
  */
 public class AdmHistoriasUsuarioTest {
 
-    public AdmHistoriasUsuarioTest() {
-    }
-
-    @Test
+	@Test
     public void admHistoriasDebeRegistrarHistoriaUsuario(){
-        // Arrange
-        //   Definir los datos de pruebas
-        String codigo = "HU001";
-        String descripcion = "Realizar el requerimiento N1";
-        String fecha = "24/03/2012";
-        String usuario = "P0001";
-        String estado = "Asignado";
-        // Act
-        //  Ejecutar los métodos a probar
-        AdmHistoriasUsuario adm = new AdmHistoriasUsuario();
-        adm.registrarHistoriaUsuario(codigo, descripcion, fecha,usuario, estado);
-        // Assert
-        HistoriaUsuario historia = adm.buscarHistoria(codigo);
-        assertNotNull(historia);
-        assertTrue(historia.getCodigo().equals(codigo));
-        assertTrue(historia.getDescripcion().equals(descripcion));
-        assertTrue(historia.getFecha().equals(fecha));
-        assertTrue(historia.getUsuario().equals(usuario));
-        assertTrue(historia.getEstado().equals(estado));
+    	
+    	  int iIDhistoria = 0001;
+	        int iIdProducto = 0001;
+	        int iOrdenPrioridadEstimada = 1;
+	        int iOrdenPrioridadReal = 3;
+	        String cEstado = "A";
+	        int iDuracionDias=5;
+	        float fCosto=12;
+	        // Act
+	        //  Ejecutar los métodos a probar
+	        AdmHistoriasUsuario adm = new AdmHistoriasUsuario();
+	        adm.registrarHistoriaUsuario(iIDhistoria, iIdProducto, iOrdenPrioridadEstimada,iOrdenPrioridadReal, cEstado, iDuracionDias, fCosto);
+	        
+	        // Arrange
+	        //   Definir los datos de pruebas
+	        iIDhistoria = 0002;
+	        iIdProducto = 0001;
+	        iOrdenPrioridadEstimada = 1;
+	        iOrdenPrioridadReal = 3;
+	        cEstado = "";
+	        iDuracionDias=5;
+	        fCosto=12;
+	        // Act
+	        //  Ejecutar los métodos a probar	   
+	        adm.registrarHistoriaUsuario(iIDhistoria, iIdProducto, iOrdenPrioridadEstimada,iOrdenPrioridadReal, cEstado, iDuracionDias, fCosto);
+	        	                
+        
+	        // Arrange
+	        //   Definir los datos de pruebas
+	        iIDhistoria = 0002;
+	        iIdProducto = 0001;
+	        iOrdenPrioridadEstimada = 1;
+	        iOrdenPrioridadReal = 3;
+	        cEstado = "A";
+	        iDuracionDias=5;
+	        fCosto=12;
+	        // Act
+	        //  Ejecutar los métodos a probar	   
+	        
+	        	        
+        assertTrue(adm.registrarHistoriaUsuario(iIDhistoria, iIdProducto, iOrdenPrioridadEstimada,iOrdenPrioridadReal, cEstado, iDuracionDias, fCosto));       
+       // adm.printHistoriaUsuario();
     }
-
-
 }
