f075b215a6aaef493e763b37043336104be146e8
evy2586 <evy2586@4e4c0a45-7b8d-c99e-a0c6-614e66cbec3a>
2012-04-06 03:16:03 +0000

    actualizacion de la clase

--- SCRUM-GRUPO1/test/gestionscrum/HistoriaUsuario/HistoriaUsuarioTest.java
+++ SCRUM-GRUPO1/test/gestionscrum/HistoriaUsuario/HistoriaUsuarioTest.java
@@ -18,22 +18,27 @@ public class HistoriaUsuarioTest {
     public void historiaUsuarioDebeCrearseConCodigoDescripcionFecha(){
         // Arrange
         //   Definir los datos de pruebas
-        String codigo = "HU001";
-        String descripcion = "Realizar el requerimiento N1";
-        String fecha = "24/03/2012";
-        String usuario= "PO0001";
-        String estado= "Asignado"; //asignar, en progreso, finalizado
+    	  int iIDhistoria = 0001;
+	        int iIdProducto = 0001;
+	        int iOrdenPrioridadEstimada = 1;
+	        int iOrdenPrioridadReal = 3;
+	        String cEstado = "A";
+	        int iDuracionDias=5;
+	        float fCosto=12;//asignar, en progreso, finalizado
         // Act
         //  Se ejecuta el método a probar
-        HistoriaUsuario historia = new HistoriaUsuario(codigo, descripcion, fecha, usuario, estado);
+        HistoriaUsuario historia = new HistoriaUsuario(iIDhistoria, iIdProducto, iOrdenPrioridadEstimada,iOrdenPrioridadReal, cEstado, iDuracionDias, fCosto);
         // Assert
         //  Se comprueba los resultados de la ejecución
         assertNotNull(historia);
-        assertTrue(historia.getCodigo().equals(codigo));
-        assertTrue(historia.getDescripcion().equals(descripcion));
-        assertTrue(historia.getFecha().equals(fecha));
-        assertTrue(historia.getUsuario().equals(usuario));
-        assertTrue(historia.getEstado().equals(estado));
+        assertTrue(historia.getiIDhistoria()==(iIDhistoria));
+        assertTrue(historia.getiIdProducto()==(iIdProducto));
+        assertTrue(historia.getiOrdenPrioridadEstimada()==(iOrdenPrioridadEstimada));
+        assertTrue(historia.getiOrdenPrioridadReal()==(iOrdenPrioridadReal));
+        assertTrue(historia.getcEstado().equals(cEstado));
+        assertTrue(historia.getiDuracionDias()==(iDuracionDias));
+        assertTrue(historia.getfCosto()==(fCosto));
+        
     }
 
 
