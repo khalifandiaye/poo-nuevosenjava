7b2d114256b40e8521546c3ad61381a695df407d
evy2586 <evy2586@4e4c0a45-7b8d-c99e-a0c6-614e66cbec3a>
2012-04-06 03:16:44 +0000

    actualizacion de la clase

--- SCRUM-GRUPO1/src/gestionscrum/HistoriaUsuario/HistoriaUsuario.java
+++ SCRUM-GRUPO1/src/gestionscrum/HistoriaUsuario/HistoriaUsuario.java
@@ -4,89 +4,120 @@ package gestionscrum.HistoriaUsuario;
  * @author Evelyn Cordova
  */
 public class HistoriaUsuario {
-    private String codigo;
-    private String descripcion;
-    private String fecha;
-    private String usuario;
-    private String estado;
-
-
-    public HistoriaUsuario(String codigo, String descripcion, String fecha,String usuario, String estado) {
-        this.codigo = codigo;
-        this.descripcion = descripcion;
-        this.fecha = fecha;
-        this.usuario= usuario;
-        this.estado = estado;
+    private int iIDhistoria;
+    private int iIdProducto;
+    private int iOrdenPrioridadEstimada;
+    private int iOrdenPrioridadReal;
+    private String cEstado;
+    private int iDuracionDias;
+    private float fCosto;
+    
+    public HistoriaUsuario(int viIDhistoria, int viIdProducto, int viOrdenPrioridadEstimada,int viOrdenPrioridadReal, String vcEstado, int viDuracionDias, float vfCosto) {
+        this.iIDhistoria = viIDhistoria;
+        this.iIdProducto = viIdProducto;
+        this.iOrdenPrioridadEstimada = viOrdenPrioridadEstimada;
+        this.iOrdenPrioridadReal= viOrdenPrioridadReal;
+        this.cEstado = vcEstado;
+        this.iDuracionDias=viDuracionDias;
+        this.fCosto=vfCosto;
     }
 
     /**
-     * @return the codigo
+     * @return the iIDhistoria
      */
-    public String getCodigo() {
-        return codigo;
+    public int getiIDhistoria() {
+        return iIDhistoria;
     }
 
     /**
-     * @param codigo the codigo to set
+     * @param iIDhistoria the iIDhistoria to set
      */
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
+    public void setiIDhistoria(int viIDhistoria) {
+        this.iIDhistoria = viIDhistoria;
     }
 
     /**
-     * @return the descripcion
+     * @return the iIdProducto
      */
-    public String getDescripcion() {
-        return descripcion;
+    public int getiIdProducto() {
+        return iIdProducto;
     }
 
     /**
-     * @param descripcion the descripcion to set
+     * @param iIdProducto the iIdProducto to set
      */
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+    public void setiIdProducto(int viIdProducto) {
+        this.iIdProducto = viIdProducto;
     }
 
     /**
-     * @return the fecha
+     * @return the iOrdenPrioridadEstimada
      */
-    public String getFecha() {
-        return fecha;
+    public int getiOrdenPrioridadEstimada() {
+        return iOrdenPrioridadEstimada;
     }
 
     /**
-     * @param fecha the fecha to set
+     * @param iOrdenPrioridadEstimada the iOrdenPrioridadEstimada to set
      */
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
+    public void setiOrdenPrioridadEstimada(int viOrdenPrioridadEstimada) {
+        this.iOrdenPrioridadEstimada = viOrdenPrioridadEstimada;
     }
     
     /**
-     * @return the usuario
+     * @return the iOrdenPrioridadReal
      */
-    public String getUsuario() {
-        return usuario;
+    public int getiOrdenPrioridadReal() {
+        return iOrdenPrioridadReal;
     }
 
     /**
-     * @param fecha the usuario to set
+     * @param iOrdenPrioridadReal the iOrdenPrioridadReal to set
      */
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
+    public void setiOrdenPrioridadReal(int viOrdenPrioridadReal) {
+        this.iOrdenPrioridadReal = viOrdenPrioridadReal;
     }
     
     /**
-     * @return the estado
+     * @return the cEstado
      */
-    public String getEstado() {
-        return estado;
+    public String getcEstado() {
+        return cEstado;
     }
 
     /**
-     * @param fecha the estado to set
+     * @param cEstado the cEstado to set
      */
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public void setcEstado(String vcEstado) {
+        this.cEstado = vcEstado;
+    }
+
+    /**
+     * @return the cEstado
+     */
+    public int getiDuracionDias() {
+        return iDuracionDias;
+    }
+
+    /**
+     * @param cEstado the cEstado to set
+     */
+    public void setiDuracionDias(int viDuracionDias) {
+        this.iDuracionDias = viDuracionDias;
+    }
+
+    /**
+     * @return the cEstado
+     */
+    public float getfCosto() {
+        return fCosto;
+    }
+
+    /**
+     * @param cEstado the cEstado to set
+     */
+    public void setfCosto(float vfCosto) {
+        this.fCosto = vfCosto;
     }
 
 }
