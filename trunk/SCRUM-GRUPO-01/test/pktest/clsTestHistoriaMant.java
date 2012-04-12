3fd887c6e44d3a45c28010d3c8d75266c02f9bd9
evy2586 <evy2586@4e4c0a45-7b8d-c99e-a0c6-614e66cbec3a>
2012-04-12 22:51:05 +0000

    se agrego los campos de auditoria y de descripcion de historia

--- trunk/SCRUM-GRUPO-01/test/pktest/clsTestHistoriaMant.java
+++ trunk/SCRUM-GRUPO-01/test/pktest/clsTestHistoriaMant.java
@@ -2,58 +2,89 @@ package pktest;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+import java.util.Calendar;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import pksource.clsHistoria;
 import pksource.clsHistoriaMant;
+import pksource.clsProyectoMant;
 
 public class clsTestHistoriaMant {
 
 	clsHistoriaMant adm = new clsHistoriaMant();
 	int iIDhistoria;
 	int iIdProducto;
+	String vDescripcion;
 	int iOrdenPrioridadEstimada;
 	int iOrdenPrioridadReal;
 	String cEstado;
 	int iDuracionDias;
 	float fCosto;
-
+	Date dFechaCreacion;
+	int iIDUsuarioCreacion;
+	Date dFechaModificacion;
+	int iIDUsuarioModificacion;
+	
+	clsProyectoMant oProyectoMant = new clsProyectoMant();
+	Calendar c1 = Calendar.getInstance();
+	java.util.Date Fecha = c1.getTime();
+	
 	@Before
-	public void Carga() {
+	public void Carga() {		
+		
 		// Registro 1
-		adm.pRegistrarHistoria(1, 1, 1, 1, "A", 5, 1000);
+		adm.pRegistrarHistoria(1, 1, "Requerimiento 1", 1, 1, "A", 5, 0f,Fecha, 1, Fecha, 1);
 		// Registro 2
-		adm.pRegistrarHistoria(2, 1, 2, 2, "A", 10, 2000);
+		adm.pRegistrarHistoria(2, 1, "Requerimiento 2", 2, 2, "A", 10, 0f,Fecha, 1, Fecha, 1);
 		// Registro 3
-		adm.pRegistrarHistoria(3, 1, 3, 3, "P", 15, 3000);
+		adm.pRegistrarHistoria(3, 1, "Requerimiento 3", 3, 3, "P", 15, 0f,Fecha, 1, Fecha, 1);
+
+		oProyectoMant.pAgregar(1, "GESTION DE PROVEEDORES", "WONG",
+				"03/03/2012", "03/03/2012", 1, 1);
+		oProyectoMant.pAgregar(2, "MODULO DE MANTENIMIENTO", "UPC",
+				"03/03/2012", "03/03/2012", 1, 1);
+		oProyectoMant.pAgregar(3, "CONTROL DE VISITAS", "SARITA COLONIA",
+				"03/03/2012", "03/03/2012", 1, 1);
 	}
 
 	@Test
-	public void RegistrarHistoria() {
-		System.out.println("Registro de Historia de Usuario");		
+	public void RegistrarHistoriaConIDProductoInexistente() {
+		System.out.println("Registro de Historia de Usuario");
 
 		// Registro 4
-		iIDhistoria = 0004;
-		iIdProducto = 0001;
+		iIDhistoria = 4;
+		iIdProducto = 4;
+		vDescripcion="Requerimiento 4";
 		iOrdenPrioridadEstimada = 1;
 		iOrdenPrioridadReal = 3;
 		cEstado = "A";
 		iDuracionDias = 5;
-		fCosto = 12;
+		fCosto = 12.00f;
+		dFechaCreacion=Fecha;
+		iIDUsuarioCreacion=1;
+		dFechaModificacion=Fecha;
+		iIDUsuarioModificacion=1;
 
 		// Registra, valida que no exista y que se hayan ingresado los datos
 		// necesarios.
-		assertTrue(adm.pRegistrarHistoria(iIDhistoria, iIdProducto,
-				iOrdenPrioridadEstimada, iOrdenPrioridadReal, cEstado,
-				iDuracionDias, fCosto));
+		if (!oProyectoMant.faBuscarPk(iIdProducto).equals(null)) {
+			assertTrue(adm.pRegistrarHistoria(iIDhistoria, iIdProducto,vDescripcion,
+					iOrdenPrioridadEstimada, iOrdenPrioridadReal, cEstado,
+					iDuracionDias, fCosto,dFechaCreacion,iIDUsuarioCreacion,dFechaModificacion,iIDUsuarioModificacion));
+		} else {
+			System.out.println("Producto no existe.");
+		}
+
 		// adm.printHistoriaUsuario();
 	}
 
 	@Test
 	public void BuscarHistoria() {
 		System.out.println("Busqueda de Historia de Usuario");
-		
+
 		// Buscar un registro
 		clsHistoria historia = adm.buscarHistoria(0001);
 		assertNotNull(historia);
@@ -72,21 +103,27 @@ public class clsTestHistoriaMant {
 	public void ActualizarHistoria() {
 
 		System.out.println("Actualizacion de Historia de Usuario");
-	
+
 		adm.printHistoriaUsuario();
 
 		// historia a actualizar
 		iIDhistoria = 0002;
 		iIdProducto = 0001;
+		vDescripcion="Requerimiento 4";
 		iOrdenPrioridadEstimada = 1;
 		iOrdenPrioridadReal = 3;
 		cEstado = "E";
 		iDuracionDias = 5;
 		fCosto = 12;
+		dFechaCreacion=Fecha;
+		iIDUsuarioCreacion=1;
+		dFechaModificacion=Fecha;
+		iIDUsuarioModificacion=1;
 
-		assertTrue(adm.actualizarHistoriaUsuario(iIDhistoria, iIdProducto,
+
+		assertTrue(adm.actualizarHistoriaUsuario(iIDhistoria, iIdProducto,vDescripcion,
 				iOrdenPrioridadEstimada, iOrdenPrioridadReal, cEstado,
-				iDuracionDias, fCosto));
+				iDuracionDias, fCosto,dFechaCreacion,iIDUsuarioCreacion,dFechaModificacion,iIDUsuarioModificacion));
 
 		System.out.println("Despues de Actualización:");
 
@@ -96,7 +133,7 @@ public class clsTestHistoriaMant {
 	@Test
 	public void EliminarHistoriaUsuario() {
 		System.out.println("Eliminación de Historia de Usuario");
-		
+
 		adm.printHistoriaUsuario();
 
 		assertTrue(adm.eliminarHistoriaUsuario(0003));
